c6b8f5d28011368b87f196f7c5f7fc1ebaf7f8b2
sifatullahsu <personal.sifat@gmail.com>
2023-03-21 16:17:35 +0600

    data update

--- v1/routes/user_handler.js
+++ v1/routes/user_handler.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const { userValidation } = require('../../utilities/validations');
 const userController = require('../controllers/user_controller');
 const router = express.Router();
 
@@ -7,7 +6,6 @@ const router = express.Router();
 router.get('/random', userController.getRendomUser);
 router.get('/all', userController.getAllUser);
 router.post('/save', userController.saveUser);
-
 router.patch('/update', userController.updateUser);
 router.patch('/bulk-update', userController.updateBulkUser);
 router.delete('/delete', userController.deleteUser);
